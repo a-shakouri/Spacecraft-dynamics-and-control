f5cd895cea1f0df39050a79321f841f5986239c9
shakouri-ta <74881618+shakouri-ta@users.noreply.github.com>
2020-12-05 09:31:05 +0330

    Update rk4.m

--- rk4.m
+++ rk4.m
@@ -17,19 +17,18 @@ switch nargin
         xd2=dyn(x+delta/2*xd1);
         xd3=dyn(x+delta/2*xd2);
         xd4=dyn(x+delta*xd3);
-        xnew=x+delta/6*(xd1+2*xd2+2*xd3+xd4);
     case 2 %If only the first two arguments are specified
         xd1=dyn(x,p);
         xd2=dyn(x+delta/2*xd1);
         xd3=dyn(x+delta/2*xd2);
         xd4=dyn(x+delta*xd3);
-        xnew=x+delta/6*(xd1+2*xd2+2*xd3+xd4);
     otherwise
         xd1=dyn(x,p);
         xd2=dyn(x+delta/2*xd1,p);
         xd3=dyn(x+delta/2*xd2,p);
         xd4=dyn(x+delta*xd3,p);
-        xnew=x+delta/6*(xd1+2*xd2+2*xd3+xd4);
 end
+
+xnew=x+delta/6*(xd1+2*xd2+2*xd3+xd4);
 
 end
