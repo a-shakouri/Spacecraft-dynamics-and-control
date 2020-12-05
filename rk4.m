6b09dfbb5a4457a34da880de98806fda6f579245
shakouri-ta <74881618+shakouri-ta@users.noreply.github.com>
2020-12-05 08:58:47 +0330

    Add files via upload

---
+++ rk4.m
@@ -0,0 +1,21 @@
+function xnew=rk4(x,delta,p)
+
+%Runge-Kutta integration
+
+% Input(s):
+%           x:     State vector at t.
+%           delta: Integration time step.
+%           p:     Vector of additional parameters.
+%
+% Output(s):
+%           xnew:  State vector at t+delta.
+
+
+
+xd1=dyn(x,p);
+xd2=dyn(x+delta/2*xd1,p);
+xd3=dyn(x+delta/2*xd2,p);
+xd4=dyn(x+delta*xd3,p);
+xnew=x+delta/6*(xd1+2*xd2+2*xd3+xd4);
+
+end
